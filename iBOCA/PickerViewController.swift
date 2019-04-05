6dba7c9c079ebd6cefc5012560224bdb2a9027c0
Dat Huynh <hk.dat94@gmail.com>
2019-04-05 18:00:01 +0700

    #4: As a user, I should be able to choose with dropdown options for Gender, Age, Education, Ethnicity, and Race and should be visible at all times

---
+++ iBOCA/PickerViewController.swift
@@ -0,0 +1,67 @@
+//
+//  PickerViewController.swift
+//  iBOCA
+//
+//  Created by Dat Huynh on 4/5/19.
+//  Copyright © 2019 sunspot. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    var datasource: [String] = []
+    
+    var didSelect: ((_ selectedValue: String, _ index: Int) -> ())?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func CancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func DonePressed(_ sender: Any) {
+        guard datasource.count > 0 else {return}
+        let selectedRow = pickerView.selectedRow(inComponent: 0)
+        let selectedValue = datasource[selectedRow]
+        didSelect?(selectedValue, selectedRow)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
+extension PickerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return datasource.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.textAlignment = .center
+        }
+        pickerLabel?.font = UIFont.systemFont(ofSize: 30)
+        pickerLabel?.text = datasource[row]
+        pickerLabel?.textColor = UIColor.black
+        
+        return pickerLabel!
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+
+}
