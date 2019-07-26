b79ff8bed57250de031be7531b93baf5ef8fe7ff
Hung Luu <hung@goldfishcode.com>
2019-07-26 11:28:20 +0700

    - [Result Screen] Fix missing header bar on iOS 11.2

--- iBOCA/ResultsViewController.swift
+++ iBOCA/ResultsViewController.swift
@@ -29,8 +29,6 @@ class ResultsViewController: ViewController {
             if let hView = UINib.init(nibName: ResultsHeaderView.identifier(), bundle: nil).instantiate(withOwner: self, options: nil).first as? ResultsHeaderView {
                 self.headerView = hView
                 self.tableView.tableHeaderView = self.headerView
-                
-                tableView.layoutTableHeaderView()
             }
         }
     }
@@ -192,33 +190,5 @@ extension ResultsViewController: ResultsHeaderSectionViewDelegate {
         tableView.reloadSections([section], with: .fade)
     }
     
-}
-
-
-
-extension UITableView {
-    
-    func layoutTableHeaderView() {
-        
-        guard let headerView = self.tableHeaderView else { return }
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let headerWidth = headerView.bounds.size.width;
-        let temporaryWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "[headerView(width)]", options: NSLayoutFormatOptions(rawValue: UInt(0)), metrics: ["width": headerWidth], views: ["headerView": headerView])
-        
-        headerView.addConstraints(temporaryWidthConstraints)
-        
-        headerView.setNeedsLayout()
-        headerView.layoutIfNeeded()
-        
-        let headerSize = headerView.systemLayoutSizeFitting(UILayoutFittingCompressedSize)
-        let height = headerSize.height
-        var frame = headerView.frame
-        
-        frame.size.height = height
-        headerView.frame = frame
-        
-        self.tableHeaderView = headerView
-    }
 }
 
